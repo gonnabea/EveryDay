a8e2475cb96ae6a09a90f111b1f3edcc760e2b3d
Choi Jiwon <winning1993@naver.com>
2020-04-09 06:47:52 +0900

    텍스트 효과 색 변경

--- home.js
+++ home.js
@@ -2,6 +2,8 @@ const container1 = document.getElementById("container1");
 
 function handleClick(){
     container1.style.animation="slidePage 0.5s ease-in-out forwards";
+    container1.style.cursor="default";
+    
 }
 
 
