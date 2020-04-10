81f5e84a4b773be82e1fdefe070e5f5b8026f4e6
Choi Jiwon <winning1993@naver.com>
2020-04-10 16:28:39 +0900

    3D 스탑워치 준비

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const container1 = document.getElementById("container1");
+const textBackground = document.getElementById("textBackground");
+const textBgEffect = document.getElementById("textBgEffect");
+const author = document.getElementById("author");
+const mainArea = document.getElementById("mainArea");
 
 function handleClick(){
-    container1.style.animation="slidePage 0.5s ease-in-out forwards";
+    textBackground.style.animation="slidePage 0.5s ease-in-out forwards";
+    textBackground.style.position="absolute";
+    textBgEffect.style.display= "none";
+    author.style.animation="slideAuthor 0.5s ease-in-out forwards";
+    author.style.opacity="1";
     container1.style.cursor="default";
     
+    mainArea.style.display="block";
 }
 
 
